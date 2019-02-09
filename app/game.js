9854712bae9f9479125e8a4be7c841017af5c9fa
seancmaginnis <seancmaginnis@gmail.com>
2019-02-09 14:00:20 -0700

    items added with modifiers

--- app/game.js
+++ app/game.js
@@ -2,44 +2,79 @@ let target = {
   name: "steven",
   health: 100,
   hits: 0,
-  items: ['hpotion', 'steriod', 'pwn']
+  items: []
 
 }
 let items = {
-  hpotion: { name: 'hpotion', modifier: 2, discription: 'Heals' },
+  hpotion: { name: 'hpotion', modifier: -2, discription: 'Heals' },
   steroid: { name: 'steroid', modifier: 4, discription: 'RAGE' },
   pwn: { name: 'pwn', modifier: 100, discription: 'WIN' }
 
-
 }
-let kick = 10
+let kick = 10,
+
+  modifierActivated = false
 
 function slap() {
   if (target.health >= 1) {
-    target.health--;
+    let damage = 1 + addMods();
+    target.health -= damage;
     target.hits++
   }
   update()
 }
 function roundhouse() {
   if (target.health >= 1) {
-    target.health -= kick;
+    let damage = 10 + addMods();
+    target.health -= damage;
     target.hits++
   }
   update()
 }
 
-function crobar() {
+function crowbar() {
   if (target.health >= 1) {
     target.health -= 20;
     target.hits++;
   }
   update()
 }
+function sshot() {
+  if (modifierActivated == false) {
+    target.items.push(items.steroid);
+    modifierActivated = true;
+  }
+  update()
+}
+function potion() {
+  if (modifierActivated == false) {
+    target.items.push(items.hpotion);
+    modifierActivated = true;
+  }
+  update()
+}
+function pwn() {
+  if (modifierActivated == false) {
+    target.items.push(items.pwn);
+    modifierActivated = true
+  }
+}
+function addMods() {
+  let modifierTotal = 0;
+  for (let i = 0; i < target.items.length; i++) {
+    modifierTotal = target.items[i].modifier;
+  }
+  return modifierTotal
+}
 
 function update() {
   document.getElementById('name').innerText = `${target.name}`
-  document.getElementById('health').innerText = `${target.health}`
   document.getElementById('hits').innerText = `${target.hits}`
+  if (target.health <= 0) {
+    document.getElementById('health').innerText = 'YOU WIN'
+  }
+  else {
+    document.getElementById('health').innerText = `${target.health}`
+  }
 }
 update()
